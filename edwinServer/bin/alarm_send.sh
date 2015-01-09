b85b7499855aa9cf4c59bf926429a060b6e16fe4
harryliu <harrychinese@gmail.com>
2015-01-09 22:08:33 +0800

    fix the sever side bash scripts

--- edwinServer/bin/alarm_send.sh
+++ edwinServer/bin/alarm_send.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #set python executable 
-export PYTHON_BIN=/home/user1/pythonenv/python27/scripts/python
+PYTHON_BIN=/home/user1/pythonenv/python27/scripts/python
 
 # base path, it is the parent of root package path  
-export BASE_PATH=/home/user1/trunk/workspace/Edwin
+BASE_PATH=/home/user1/trunk/workspace/Edwin
 
 # set your python script file. Please be noticed to trim the tailed space of file name 
-export MY_PY_SCRIPT=alarm_send.py
+MY_PY_SCRIPT=alarm_send.py
 
 
 #=================================
 # do not change the following code
 #=================================
-export PYTHONPATH=$BASE_PATH
+PYTHONPATH=$BASE_PATH
 
-export SCRIPT_PATH=$BASE_PATH/edwinServer/scripts
-export BIN_PATH=$BASE_PATH/edwinServer/bin
-export LOCK_FILE=$BIN_PATH/locks/$MY_PY_SCRIPT.lock
-export MY_PY_FILE=$SCRIPT_PATH/$MY_PY_SCRIPT
+SCRIPT_PATH=$BASE_PATH/edwinServer/scripts
+BIN_PATH=$BASE_PATH/edwinServer/bin
+LOCK_FILE=$BIN_PATH/locks/$MY_PY_SCRIPT.lock
+MY_PY_FILE=$SCRIPT_PATH/$MY_PY_SCRIPT
 
 cnt=`ls $LOCK_FILE|wc -l`
 if [ ${cnt} -lt 1 ]
